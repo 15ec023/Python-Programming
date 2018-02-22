39d82b9a064468fb5a5847540259a6dee282cd11
15ec023 <priya98.pb@gmail.com>
2018-02-22 20:02:05 +0530

    Update Even odd.py

--- Beginner Level/Even odd.py
+++ Beginner Level/Even odd.py
@@ -1,6 +1,7 @@
-a=int(input())
-if(1<=a<=100000)
- if(a%2==0):
-  print("Even")
- else:
-  print("odd")
+ a=int(input())
+    if(a>1 and a<100000):
+     if(a%2==0):
+             print("Even")
+     else:
+             print("odd")
+
